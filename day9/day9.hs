f1ed435eecc7d1cbab566ead8219dcea8045879d
Seholm <filipslottner@gmail.com>
2016-12-22 12:53:27 +0100

    finished part 2 on day 9

--- day9/day9.hs
+++ day9/day9.hs
@@ -5,6 +5,7 @@ import Data.List
 
 main = do str <- readFile "day9input.txt"
           print $ (length $ decompress str) -1
+          print $ (decompress1 str)-1
 
 decompress :: String -> String
 decompress [] = []
@@ -13,3 +14,20 @@ decompress ('(':xs) = (concat (replicate (read (amounts !! 1) :: Int) (take (rea
                                 amountToDrop = (length (takeWhile (/=')') xs)) +1
                                 amounts = splitOn "x" (takeWhile (/=')') xs)
 decompress (x:xs)   = x:decompress xs
+
+--part2
+
+decompress1 :: String -> Int
+decompress1 [] = 0
+decompress1 ('(':xs) = decompressxTimes (take ((read (amounts !! 0) :: Int)+ amountToDrop) ('(':xs) ) +
+                              decompress1 (drop ((amountToDrop-1) + (read (amounts !! 0) :: Int)) xs) where
+                                amountToDrop = (length (takeWhile (/=')') xs)) +2
+                                amounts = splitOn "x" (takeWhile (/=')') xs)
+decompress1 (x:xs)   = (decompress1 xs)+1
+
+decompressxTimes :: String -> Int
+decompressxTimes []       = 0
+decompressxTimes ('(':xs) =  times * decompress1 (drop amountToDrop (xs))  where
+                              amountToDrop = (length (takeWhile (/=')') xs)) +1
+                              amounts = splitOn "x" (takeWhile (/=')') xs)
+                              times = read (amounts !! 1) :: Int
